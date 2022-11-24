514eed0ab11c3a0ce7c7e6ba36c41c618e95de75
Arath Duran <66040055+DuranArath150@users.noreply.github.com>
2022-11-24 13:14:35 -0800

    agregar paginas

--- routes/routes.js
+++ routes/routes.js
@@ -18,3 +18,31 @@ router.get('/', (req, res) => {
 router.get('/directorio', (req, res) => {
     res.render('pages/directorio');
 });
+
+router.get('/echeverialilacina', (req, res) => {
+    res.render('pages/echeverialilacina');
+});
+
+router.get('/echeverialola', (req, res) => {
+    res.render('pages/echeverialola');
+});
+
+router.get('/echeveriapeacockii', (req, res) => {
+    res.render('pages/echeveriapeacockii');
+});
+
+router.get('/echeveriapulvinata', (req, res) => {
+    res.render('pages/echeveriapulvinata');
+});
+
+router.get('/echeveriasecunda', (req, res) => {
+    res.render('pages/echeveriasecunda');
+});
+
+router.get('/echeveriasetosa', (req, res) => {
+    res.render('pages/echeveriasetosa');
+});
+
+router.get('/echeveriashaviana', (req, res) => {
+    res.render('pages/echeveriashaviana');
+});
